cdadc34bcab028b681b91209d650cad9f38470bc
Malasidze <andriamalasidze88@gmail.com>
2026-06-17 20:12:36 +0400

    დავალება 7

--- HomeWork3/Program.cs
+++ HomeWork3/Program.cs
@@ -4,63 +4,81 @@
     {
         static void Main(string[] args)
         {
-            //davaleba1
-
-            //int[] arr1 = { 1, 2, 3 };
-            //int[] arr2 = { 4, 5, 6 };
-
-            //int[] resultArr = new int[arr1.Length + arr2.Length];
+            //დავალება 1
 
 
-            //for (int i = 0; i < arr1.Length; i++)
+            //Console.WriteLine(power(2, 3));
+
+
+
+            //int power(int num1, int num2)
             //{
-            //    resultArr[i] = arr1[i];
-            //}
-
-
-            //for (int i = 0; i < arr2.Length; i++)
-            //{
-            //    resultArr[arr1.Length + i] = arr2[i];
-            //}
-
-
-            //Console.Write("result arr [ ");
-            //for (int i = 0; i < resultArr.Length; i++)
-            //{
-            //    Console.Write(resultArr[i]);
-
-
-            //    if (i < resultArr.Length - 1)
+            //    int result = num1;
+            //    for (int i = 1; i < num2; i++)
             //    {
-            //        Console.Write(", ");
+            //        result *= num1; 
             //    }
+
+            //    return result;
             //}
 
-            //Console.WriteLine(" ]");
 
 
 
             //davaleba2
-            int[] arr1 = [3, 5, -4, 8, 11, 1, -1, 6];
-            Console.Write("შეიყვანეთ სამიზნე ჯამი: ");
 
-            //bool isValid = int.TryParse(Console.ReadLine(), out int num);
-            int targetSum = 7;
+            //bool IsPrime(int num)
+            //{
 
-            for (int i = 0; i < arr1.Length; i++) { 
-            for(int j = 1+i; j< arr1.Length; j++)
-                {
-                    if (arr1[i]+ arr1[j]== targetSum)
-                    {
-                        Console.WriteLine("[" + arr1[i] + ", " + arr1[j] + "]");
-                    }
-                }
-                 }
-        
+            //    for (int i = 2; i < num; i++)
+            //    {
+            //        if (num % i == 0)
+            //        {
+            //            return false;
+            //        }
+            //    }
 
+            //    return true;
+
+            //}
 
 
+            //Console.WriteLine("Ennter numebe");
+            //bool isValid = int.TryParse(Console.ReadLine(), out int userNum);
 
+            //Console.WriteLine(IsPrime(userNum) ? "primitive" : "conpose");
+
+
+            //davaleba3
+
+            //bool IsPailndrome(string pal )
+            //{
+            //	if(pal.Length < 2)
+            //	{
+            //		return true;
+            //	}
+
+            //	if (pal[0] != pal[pal.Length - 1])
+            //	{
+            //		return false;
+            //	}
+
+            //	string middle = pal.Substring(1, pal.Length-2);
+
+            //	return  IsPailndrome(middle);
+            //}
+
+
+            //davaleba 5
+            //int Pow(int num, int powerer)
+            //{
+            //	if(powerer == 1)
+            //	{
+            //		return num;
+            //	}
+
+            //	return num * Pow(num, powerer - 1);
+            //}
 
 
         }
